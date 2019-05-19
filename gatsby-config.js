78401b6574882aa3244ec93f58a49fb3048b1bec
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-05-19 10:23:21 -0500

    practice using graphql and graphiql

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,17 +4,24 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
- module.exports = {
-   siteMetadata: {
-     title: `Pandas Eating Lots`
-   },
-   plugins: [
-     `gatsby-plugin-emotion`,
-     {
-       resolve: `gatsby-plugin-typography`,
-       options: {
-         pathToConfigModule: `src/utils/typography`,
-       },
-     },
-   ],
- }
+module.exports = {
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
